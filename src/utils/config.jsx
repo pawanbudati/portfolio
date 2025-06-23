20008fdfb9f166571bf442046bbb7c197a81e751
pawanbudati <pawan1122143@gmail.com>
2025-06-23 20:24:35 +0530

    Modified icons

--- src/utils/config.jsx
+++ src/utils/config.jsx
@@ -1,13 +1,13 @@
-import { AiOutlineHome, AiOutlineUser, AiOutlineBook, AiOutlineCode, AiOutlineMessage, AiOutlineRadiusSetting, AiOutlineUnorderedList } from 'react-icons/ai';
+import { AiOutlineHome, AiFillPhone, AiOutlineUser, AiOutlineOpenAI, AiOutlineProject, AiOutlineRead, AiOutlineSwitcher } from 'react-icons/ai';
 
 const navbarConfig = [
-    { id:0, icon: AiOutlineHome, route: "#", title: "Home", tooltip:"Home" },
+    { id:1, icon: AiOutlineHome, route: "#", title: "Home", tooltip:"Home" },
     { id:2, icon: AiOutlineUser, route: "#about", title: "About", tooltip:"About" },
-    { id:3, icon: AiOutlineBook, route: "#experience", title: "Experience", tooltip:"Experience" },
-    { id:6, icon: AiOutlineUnorderedList, route: "#skills", title: "Skills", tooltip:"Skills" },
-    { id:4, icon: AiOutlineCode, route: "#projects", title: "Projects", tooltip:"Projects" },
-    { id:7, icon: AiOutlineMessage, route: "#education", title: "Education", tooltip:"Education" },
-    { id:5, icon: AiOutlineCode, route: "#contact", title: "Contact", tooltip:"Contact" },
+    { id:3, icon: AiOutlineSwitcher, route: "#experience", title: "Experience", tooltip:"Experience" },
+    { id:6, icon: AiOutlineOpenAI, route: "#skills", title: "Skills", tooltip:"Skills" },
+    { id:4, icon: AiOutlineProject, route: "#projects", title: "Projects", tooltip:"Projects" },
+    { id:7, icon: AiOutlineRead, route: "#education", title: "Education", tooltip:"Education" },
+    { id:5, icon: AiFillPhone, route: "#contact", title: "Contact", tooltip:"Contact" },
 ]
 
 
